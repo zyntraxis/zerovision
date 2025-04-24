d6fd44c60646042f8c95d0b48db575c554aa521e
zyntraxis <armenbaghdasaryancoding@gmail.com>
2025-04-24 20:00:55 +0400

    ready!

---
+++ README.md
@@ -0,0 +1,2 @@
+sudo git clone https://github.com/zyntraxis/zerovision.git /usr/local/bin/zerovision && cd ~ && cd /usr/local/bin/zerovision/scripts && ./config.sh
+
--- scripts/scan_vulns.sh
+++ scripts/scan_vulns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXEC_PATH="/usr/local/bin/zerovision"
+EXEC_PATH="/usr/local/bin/zerovision/build/zerovision"
 
 echo "[*] Starting full vulnerability scan..."
 $EXEC_PATH
